5485018afcb4507cc6bce51ebcba6687ba99bc98
SaeedMoghimzade <smmohebby@gmail.com>
2025-11-01 20:32:43 +0330

    jwt

--- src/api/score/controllers/score.js
+++ src/api/score/controllers/score.js
@@ -1,9 +1,25 @@
-'use strict';
-
-/**
- * score controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::score.score');
+module.exports = createCoreController('api::score.score', ({ strapi }) => ({
+  async create(ctx) {
+    // دریافت اطلاعات کاربر از JWT
+    const user = ctx.state.user;
+
+    if (!user) {
+      return ctx.badRequest('کاربر احراز نشده است.');
+    }
+
+    // داده ارسالی از فرانت‌اند
+    const { data } = ctx.request.body;
+
+    // ایجاد امتیاز جدید با نسبت دادن user خودکار
+    const entry = await strapi.db.query('api::score.score').create({
+      data: {
+        ...data,
+        user: user.id,
+      },
+    });
+
+    return entry;
+  },
+}));
